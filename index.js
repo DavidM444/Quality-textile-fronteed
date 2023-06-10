0e4af51fd09749d0c5f8b731586bdd2518a431c3
DavidM444 <david541gamer@gmail.com>
2023-06-10 12:15:29 -0500

    add vista flotannte botones accion

--- index.js
+++ index.js
@@ -16,3 +16,28 @@ const slideShow = () => {
 const interval = () => {
     setInterval(slideShow, 2000)
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const openPopupButtons = document.querySelectorAll(".ope-popup");
+    const closePopupButtons = document.querySelectorAll(".close-popup");
+    const overlay = document.getElementById("popup-overlay");
+  
+    openPopupButtons.forEach(function(button) {
+      button.addEventListener("click", function() {
+        const target = button.getAttribute("data-popup-target");
+        const popup = document.getElementById(target);
+        popup.style.display = "block";
+        overlay.style.display = "block";
+      });
+    });
+  
+    closePopupButtons.forEach(function(button) {
+      button.addEventListener("click", function() {
+        const popup = button.closest(".popup");
+        popup.style.display = "none";
+        overlay.style.display = "none";
+      });
+    });
+  });
+  
